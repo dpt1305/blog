baa82a1c99453e962e3933f68f2eb0e6de4e7ef7
tungjj <tungplatin@gmail.com>
2024-07-10 10:59:15 +0700

    done: cloudwatch metric alarm

--- terraform-dynamodb/cloudwatch.tf
+++ terraform-dynamodb/cloudwatch.tf
@@ -16,21 +16,24 @@
 # }
 
 # Create the CloudWatch alarm
+# Docs: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html
 resource "aws_cloudwatch_metric_alarm" "item_count_alarm" {
     
-  alarm_name = "High-Item-Count-In-${aws_dynamodb_table.Users.name}"
-  alarm_description = "Alarm triggered if item count in ${aws_dynamodb_table.Users.name} exceeds 10"
+  alarm_name = "Item-Count-In-${aws_dynamodb_table.Users.name}"
+  alarm_description = "Alarm triggered if item count in ${aws_dynamodb_table.Users.name} more than 10"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold = 2
-  evaluation_periods = 5  # Evaluate over 2 periods (e.g., 2 minutes)
-  statistic = "Sum"
+  evaluation_periods = 1  # Evaluate over 2 periods (e.g., 2 minutes)
+  datapoints_to_alarm = 1
+
   period = 60            # Period of the metric data (e.g., 1 minute)
   namespace = "AWS/DynamoDB"
-  metric_name = "SuccessfulRequestLatency"
+  metric_name = "ReturnedItemCount"
   dimensions = {
     TableName = aws_dynamodb_table.Users.name
+    Operation = "Scan"
   }
-
+  statistic = "Sum"
 
   # Alarm actions (optional)
   # You can configure SNS notifications or Lambda function invocation here
--- terraform-dynamodb/sns.tf
+++ terraform-dynamodb/sns.tf
@@ -11,4 +11,6 @@ resource "aws_sns_topic_subscription" "send_email" {
   topic_arn = aws_sns_topic.sns_dynamo_trigger.arn
   protocol  = "email"
   endpoint  = "phutung99.kma@gmail.com"  # Replace with your verified email address
+  endpoint_auto_confirms = true
+
 }
