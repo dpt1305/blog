ba90e7b5cc43bf077bf02b5c947942e8e218bf9a
tungjj <tungplatin@gmail.com>
2024-07-12 16:01:26 +0700

    fix: bug in metric alarm's operation

--- terraform-dynamodb/cloudwatch.tf
+++ terraform-dynamodb/cloudwatch.tf
@@ -1,42 +1,20 @@
-# Get the number of items in the table using the data source
-# data "aws_dynamodb_table" "dynamo_db_table" {
-#   table_name = local.name
-# }
-# data "aws_dynamodb_table" "dynamodb_table" {
-#     name = local.name
-# }
-
-# Define the CloudWatch metric
-# resource "aws_cloudwatch_metric" "item_count_metric" {
-#   namespace = "AWS/DynamoDB"
-#   metric_name = "ItemCount"
-#   dimensions = {
-#     TableName = aws_dynamodb_table.dynamodb_table.name
-#   }
-# }
-
 # Create the CloudWatch alarm
 # Docs: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html
 resource "aws_cloudwatch_metric_alarm" "item_count_alarm" {
-    
   alarm_name = "Item-Count-In-${aws_dynamodb_table.Users.name}"
   alarm_description = "Alarm triggered if item count in ${aws_dynamodb_table.Users.name} more than 10"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold = 2
-  evaluation_periods = 1  # Evaluate over 2 periods (e.g., 2 minutes)
+  evaluation_periods = 1  
   datapoints_to_alarm = 1
-
-  period = 60            # Period of the metric data (e.g., 1 minute)
+  period = 60            
   namespace = "AWS/DynamoDB"
-  metric_name = "ReturnedItemCount"
+  metric_name = "SuccessfulRequestLatency"
   dimensions = {
     TableName = aws_dynamodb_table.Users.name
-    Operation = "Scan"
+    Operation = "GetItem"
   }
-  statistic = "Sum"
-
-  # Alarm actions (optional)
-  # You can configure SNS notifications or Lambda function invocation here
+  statistic = "SampleCount"
   actions_enabled     = "true"
   alarm_actions       = [aws_sns_topic.sns_dynamo_trigger.arn]
 }
--- terraform-dynamodb/sns.tf
+++ terraform-dynamodb/sns.tf
@@ -1,16 +1,9 @@
 resource "aws_sns_topic" "sns_dynamo_trigger" {
   name                  = "sns-dynamo-trigger"
-  # fifo_topic                  = true
-#   content_based_deduplication = true
-
-#   tags = {
-#     Name = "Lab_SNS_CloudWatch"
-#   }
 }
 resource "aws_sns_topic_subscription" "send_email" {
   topic_arn = aws_sns_topic.sns_dynamo_trigger.arn
   protocol  = "email"
   endpoint  = "phutung99.kma@gmail.com"  # Replace with your verified email address
   endpoint_auto_confirms = true
-
 }
